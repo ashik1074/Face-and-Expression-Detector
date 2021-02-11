836c16c72af65776aabe8c179b272a040213cb4f
Md. Ashikur Rahman <48981854+atvhm@users.noreply.github.com>
2021-02-11 14:58:31 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+const video = document.getElementById('video')
+
+//calling api models
+Promise.all([
+  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+  faceapi.nets.faceExpressionNet.loadFromUri('/models')
+]).then(startVideo)
+
+//initiating the video in browser
+function startVideo() {
+  navigator.getUserMedia(
+    { video: {} },
+    stream => video.srcObject = stream,
+    err => console.error(err)
+  )
+}
+
+video.addEventListener('play', () => {
+  //detection box
+  const canvas = faceapi.createCanvasFromMedia(video)
+  document.body.append(canvas)
+  const displaySize = { width: video.width, height: video.height }
+  faceapi.matchDimensions(canvas, displaySize)
+  setInterval(async () => {
+    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+    const resizedDetections = faceapi.resizeResults(detections, displaySize)
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+    faceapi.draw.drawDetections(canvas, resizedDetections)       //detecting face
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)    //detecting face structure
+    faceapi.draw.drawFaceExpressions(canvas, resizedDetections)  //detecting face expression
+  }, 100)
+})
+
+//created by Ashikur Rahman
